1d7ec04035b5b3acbdad39a43febac2f8020f9a4
林文钦 <44943235+andylinwenqin@users.noreply.github.com>
2020-10-18 14:40:33 +0800

    Add files via upload

---
+++ precache-manifest.37f93d38aee3c83a952778313a7632ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9bef3b0522372b12a6617dcc6197db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d59406268263a53be3ac",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "2534ece7b3cb6457062e",
+    "url": "/static/css/main.92a3ebff.chunk.css"
+  },
+  {
+    "revision": "d59406268263a53be3ac",
+    "url": "/static/js/2.388a63ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.388a63ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2534ece7b3cb6457062e",
+    "url": "/static/js/main.ccd326f0.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
